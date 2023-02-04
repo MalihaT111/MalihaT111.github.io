dc048dcdd1f6c5751db0ce8f9c23ab86a26b6e4b
Maliha Tasnim <90735020+MalihaT111@users.noreply.github.com>
2023-02-04 14:36:06 -0500

    Add files via upload

---
+++ BlackWingsHacks2023/script.js
@@ -0,0 +1,11 @@
+// array to store events?
+// register an event 
+function registration()
+  {
+    var username;
+    var email;
+    var password; 
+    var address; 
+    var details;
+  }
+// account creation 
